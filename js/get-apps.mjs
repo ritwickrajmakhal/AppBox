4a4928eebac882b9c374654d62e745c957393b35
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-12-08 03:47:16 +0530

    app page

--- js/get-apps.mjs
+++ js/get-apps.mjs
@@ -22,7 +22,7 @@ export const getApps = async (query = "", justStore = false) => {
       `https://raw.githubusercontent.com/LiquidGalaxyLAB/Data/refs/heads/main${app.base_url}${app.icon}`
     );
     appCard.setAttribute("title", app.name);
-    appCard.setAttribute("link", `/pages/app.html?name=${app.name}`);
+    appCard.setAttribute("link", `./pages/app.html?name=${app.name}`);
     section.appendChild(appCard);
   }
 };
